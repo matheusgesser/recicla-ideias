610aeb4aeee6d5cb17a47b4a04f7e68593527807
MatheusGesser <matheusmvg@hotmail.com>
2023-07-17 11:10:21 -0300

    fix: allow fullscreen video

--- src/pages/Project/index.jsx
+++ src/pages/Project/index.jsx
@@ -14,7 +14,7 @@ export function Project() {
         <h2 className="title">{post.title}</h2>
         <p className="description">{post.description}</p>
         <b className="difficulty">{post.difficulty}</b>
-        <iframe src={`https://www.youtube.com/embed/${post.link}?modestbranding=1&rel=0&controls=2&iv_load_policy=3&showinfo=0&cc_load_policy=1`} />
+        <iframe src={`https://www.youtube.com/embed/${post.link}?modestbranding=1&rel=0&controls=2&iv_load_policy=3&showinfo=0&cc_load_policy=1`} allowFullScreen='allowFullScreen' />
       </section>
     </main>
   )
